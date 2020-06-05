e70ab8f0fdc95a118e9e08d4889131c68d4fb508
Akshay Borana <akshayborana1@gmail.com>
2020-06-05 17:36:49 -0400

    feat: built Action Creator for authedUser.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,10 @@
 import { getInitialData } from '../utils/api';
 import { receiveUsers } from './users';
 import { receiveTweets } from './tweets';
+import { setAuthedUser } from './authedUser';
+
+// this is the hard-coded auth id...
+const AUTH_ID = 'tylermcginnis';
 
 //this function will follow redux thunk pattern, coz we want to make an async request inside this function...
 export function handleInitailaData () {
@@ -10,6 +14,7 @@ export function handleInitailaData () {
                 //take users and tweets and add them to the state of our redux store...
                 dispatch(receiveUsers(users));
                 dispatch(receiveTweets(tweets));
+                dispatch(setAuthedUser(AUTH_ID));
             })
     }
 }
