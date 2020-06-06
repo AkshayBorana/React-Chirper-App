8d401e5ceadaf5c239af3f711e337f4228914b5d
Akshay Borana <akshayborana1@gmail.com>
2020-06-06 15:28:46 -0400

    feat: built Acync Action Creator for liking a tweet and used optimistic updating for the store.

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,9 +1,38 @@
+import { saveLikeToggle } from "../utils/api";
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS';
+export const TOGGLE_TWEET = 'TOGGLE_TWEET';
 
 // RECEIVE_TWEETS Action Creator...
 export function receiveTweets (tweets) {
     return {
         type: RECEIVE_TWEETS,
         tweets
+    }
+}
+
+// TOGGLE_TWEET Action creator...
+function toggleTweet ({ authedUser, hasLiked, id }) {
+    return {
+        type: TOGGLE_TWEET,
+        id,
+        hasLiked,
+        authedUser
+    }
+}
+
+// Acync ActionCreator for tweet like.
+export function handleToggleTweet(tweetInfo) {
+    return (dispatch) => {
+        // optimistic updating to update the store...
+        dispatch(toggleTweet(tweetInfo));
+
+        // call to database...
+        return saveLikeToggle(tweetInfo)
+          .catch((e) => {
+              console.log('Error in handleToggleEvent', e);
+             // reset the toggle for the tweet in the store.
+             dispatch(toggleTweet(tweetInfo));
+             alert('There was an error liking the tweet.');
+          })
     }
 }
