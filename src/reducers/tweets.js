440be84afb0d5d46f409a18e0764ab21b4ebd4a9
Akshay Borana <akshayborana1@gmail.com>
2020-06-06 15:47:20 -0400

    feat: adding Reducer function for liking the tweet.

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,12 +1,23 @@
-import { RECEIVE_TWEETS } from '../actions/tweets';
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from '../actions/tweets';
 
 // Tweets Reducer function...
 export default function tweets (state = {}, action) {
     switch(action.type) {
         case RECEIVE_TWEETS:
             return {
-                ...state,
+                ...state, // Spreading all of the tweets on the object..
                 ...action.tweets
+            }
+        case TOGGLE_TWEET:
+            return {
+                ...state, // spreading the state...
+                [action.id]: {  // getting a particular tweet from the above state...
+                    ...state[action.id], // spreading that particular tweet...
+                    likes: action.hasLiked === true // adding or removing user from the tweet based on the action...
+                     ? state[action.id].likes.filter(id => id !== action.authedUser)
+                     : state[action.id].likes.concat([action.authedUser])
+
+                }
             }
         default:
             return state;
